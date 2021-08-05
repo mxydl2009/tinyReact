8d7624ff213743b694929013aef4bcaa441856be
mxydl2009 <mxydl2009@163.com>
2021-08-05 21:38:44 +0800

    更新过程, 更新元素的属性

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -1,13 +1,14 @@
 import mountElement from './mountElement'
+import updateNodeElement from './updateNodeElement'
 import updateTextNode from './updateTextNode'
 /**
  * 对两个虚拟DOM进行差异化比较，并更新到真实DOM上
  * @param {*} virtualDOM 当前的虚拟DOM, 需要对native虚拟DOM和组件类型的虚拟DOM进行不同的处理
  * @param {*} container 挂载点
- * @param {*} oldDOM 之前的虚拟DOM对象
+ * @param {*} oldDOM 之前的DOM对象
  */
 export default function diff(virtualDOM, container, oldDOM) {
-  let oldVirtualDOM = oldDOM && oldDOM._virtualDOM
+  let oldVirtualDOM = oldDOM ? oldDOM._virtualDOM: {}
   // 首先需要判定oldDOM是否存在（是否是首次渲染）
   if (!oldDOM) {
     // 在mountElement中处理组件类型和native类型的React元素
@@ -20,6 +21,7 @@ export default function diff(virtualDOM, container, oldDOM) {
         updateTextNode(virtualDOM, oldVirtualDOM, oldDOM)
       } else {
         // 元素节点， 更新属性
+        updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM)
       }
       // 更新子节点
       virtualDOM.props.children.forEach((child, index) => {
--- src/TinyReact/updateNodeElement.js
+++ src/TinyReact/updateNodeElement.js
@@ -1,22 +1,46 @@
-export default (newElement, virtualDOM) => {
+export default function updateNodeElement(newElement, virtualDOM, oldVirtualDOM) {
   // get props
-  const newProps = virtualDOM.props
+  const newProps = virtualDOM.props || {}
+  const oldProps = oldVirtualDOM && oldVirtualDOM.props || {}
+  // 按新的props来遍历，这样更新的属性值是新旧props共有的属性以及新的props新增的属性
   Object.keys(newProps).forEach((propName) => {
     const newPropValue = newProps[propName]
-    // 根据属性的类型来做不同的处理
-    if (propName.startsWith('on')) {
-      // 事件属性, 给元素添加事件监听函数, 这里不去实现react的事件系统了，而是直接给元素添加监听器
-      const eventName = propName.toLowerCase().slice(2)
-      newElement.addEventListener(eventName, newPropValue)
-    } else if (propName === 'value' || propName === 'checked') {
-      // 表单元素的value和checked属性无法使用setAttribute方法
-      newElement[propName] = newPropValue
-    } else if (propName !== 'children') {
-      // 不为children
-      if (propName === 'className') {
-        newElement.setAttribute('class', newPropValue)
-      } else {
-        newElement.setAttribute(propName, newPropValue)
+    const oldPropValue = oldProps[propName]
+    if (newPropValue !== oldPropValue) {
+      // 不管是首次渲染，还是更新属性值，都是在新的属性值不等于旧的属性值情况下，将属性值设为新的属性值
+      // 根据属性的类型来做不同的处理
+      if (propName.startsWith('on')) {
+        // 事件属性, 给元素添加事件监听函数, 这里不去实现react的事件系统了，而是直接给元素添加监听器
+        const eventName = propName.toLowerCase().slice(2)
+        newElement.addEventListener(eventName, newPropValue)
+        if (oldPropValue) {
+          // 删除旧的事件监听器
+          newElement.removeEventListener(eventName, oldPropValue)
+        }
+      } else if (propName === 'value' || propName === 'checked') {
+        // 表单元素的value和checked属性无法使用setAttribute方法
+        newElement[propName] = newPropValue
+      } else if (propName !== 'children') {
+        // 不为children
+        if (propName === 'className') {
+          newElement.setAttribute('class', newPropValue)
+        } else {
+          newElement.setAttribute(propName, newPropValue)
+        }
+      }
+    }
+  })
+  // 删除旧的props中的属性
+  Object.keys(oldProps).forEach(propName => {
+    if (!newProps[propName]) {
+      // 删除旧的propName属性
+      if (propName.startsWith('on')) {
+        // 事件监听器删除掉
+        const eventName = propName.toLowerCase().slice(2)
+        newElement.removeEventListener(eventName, newProps[propName])
+      } else if(propName !== 'children') {
+        // removeAttribute()可以删除value和checked属性
+        newElement.removeAttribute(propName)
       }
     }
   })
